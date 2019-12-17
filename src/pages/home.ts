bbaf419f343bbc89c639d695af3ffdb7209062a7
Léonard <leonard@bigsool.com>
2019-12-18 00:44:42 +0100

    add moving previews

--- src/pages/home.ts
+++ src/pages/home.ts
@@ -7,6 +7,14 @@ import Constants from '../constants';
 import { wrap } from '../core/errors/errors';
 import { WPCategory } from '../interfaces';
 import { fadeWith } from '../core/animations';
+import { timer } from 'rxjs';
+import { exhaustMap } from 'rxjs/operators';
+import { PaperProgressElement } from '@polymer/paper-progress';
+
+enum SwitchingState {
+    willPrev = 'prev',
+    willNext = 'next'
+};
 
 class Home extends Page {
     public static readonly is: string = 'ui-home';
@@ -23,6 +31,12 @@ class Home extends Page {
     public sculptureIndex = 0;
     @property({type: Number, reflect: false})
     public sculptureMax = 0;
+
+    @property({type: Number, reflect: false})
+    public serieProgress = 0;
+
+    /* Non-updating values */
+    private _currentAnimation: Animation;
 
     public static get styles(){
         return [
@@ -119,13 +133,62 @@ class Home extends Page {
             .preview .count .pagination {
                 display: inline;
             }
+
+            .preview .progress {
+                position: absolute;
+                bottom: 0;
+                width: 50vw;
+            }
+
+            .preview .progress paper-progress {
+                width: 50vw;
+                --paper-progress-active-color: #002FA7;
+                --paper-progress-secondary-color: var(--paper-light-blue-700);
+                --paper-progress-transition-duration: 0.08s;
+                --paper-progress-transition-timing-function: ease;
+                --paper-progress-transition-delay: 0.08s;
+            }
             `
         ];
     }
 
-    public async firstUpdated(_changedProperties: PropertyValues){
-        super.firstUpdated(_changedProperties);
+    private _setupWalk(){
+        return timer(5000, 5000).pipe(
+            exhaustMap(async () => {
+                const progress = this.shadowRoot.querySelector('#main-progress') as PaperProgressElement;
 
+                const animations = progress.getAnimations();
+                progress.classList.add('transiting');
+
+                if(this._canNext()){
+                    await this._onNextSculpture();
+                } else {
+                    let next = this.selected;
+                    
+                    if(this.selected == this._catMax()){
+                        next = 0;
+                        this._onCatClick(0);
+                    } else {
+                        this.selected++;
+                        next = this.selected;
+                    }
+
+                    await this._onCatClick(next);
+                }
+
+                for(const animation of animations){
+                    await animation.finished;
+                }
+
+                requestAnimationFrame(() => {
+                    progress.classList.remove('transiting');
+                });
+            }),
+        ).toPromise();
+    }
+
+    public async firstUpdated(_changedProperties: PropertyValues){
+        super.firstUpdated(_changedProperties);        
         const requestR = await fetch(Constants.graphql, {
 			method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -148,28 +211,79 @@ class Home extends Page {
                   }`})})
             .then(res => res.json()).catch(_ => this.dispatchEvent(wrap(_)));
 
-            this.categories = requestR.data.categories.nodes;
+        this.categories = requestR.data.categories.nodes;
 
-            this._onCatClick(0);
-            this.loaded = true;
+        await this._onCatClick(0);
+        this.loaded = true;
+
+        await this._setupWalk();
     }
 
-    private _onCatClick(idx: number){
+    private async _onCatClick(idx: number){
         this.selected = idx;
         this.sculptureIndex = 0;
         this.sculptureMax = 0;
         this.sculptureMax = this.categories[idx].sculptures.nodes.length;
         this.previewing = this.categories[idx].sculptures.nodes[this.sculptureIndex].featuredImage.sourceUrl;
-        this._fadeCurrent();
+
+        if(this._currentAnimation){
+            this._currentAnimation.cancel();
+        }
+
+        await this._fadeCurrent();
     }
 
-    private _fadeCurrent(){
+    private async _fadeCurrent(){
+        if(this._currentAnimation){
+            this._currentAnimation.cancel();
+        }
+
         const animation = fadeWith(300, true);
-        this._previewed.animate(animation.effect, animation.options);
+        this._currentAnimation = this._previewed.animate(animation.effect, animation.options);
+        await this._currentAnimation.finished;
+        this._currentAnimation = null;
     }
 
     private get _previewed(){
         return this.shadowRoot.querySelector('#previewed');
+    }
+
+    private _catMax(){
+        return this.categories.length - 1;
+    }
+
+    private _canPrev(){
+        return this.sculptureIndex-1 >= 0;
+    }
+
+    private _canNext(){
+        return this.sculptureIndex+1 <= this.sculptureMax-1;
+    }
+
+    private async _move(state: SwitchingState){
+        switch(state){
+            case SwitchingState.willPrev:
+                this.sculptureIndex--;
+                break;
+            case SwitchingState.willNext:
+                this.sculptureIndex++;
+                break;
+        }
+
+        this.previewing = this.categories[this.selected].sculptures.nodes[this.sculptureIndex].featuredImage.sourceUrl;
+        await this._fadeCurrent();   
+    }
+
+    private async _onPrevSculpture(_e?: Event){
+        if(!this._canPrev()){ return; }
+
+        await this._move(SwitchingState.willPrev);
+    }
+
+    private async _onNextSculpture(_e?: Event){
+        if(!this._canNext()){ return; }
+
+        await this._move(SwitchingState.willNext);
     }
 
     public render(): void | TemplateResult {
@@ -188,21 +302,16 @@ class Home extends Page {
                     <iron-icon icon="unfold-more"></iron-icon>
                 </div>
                 <div class="count">
-                    <iron-icon icon="chevron-left" class="${this.sculptureIndex === 0 ? 'disabled' : ''}" @click=${() => {
-                        if(this.sculptureIndex-1 >= 0){
-                            this.sculptureIndex--;
-                            this.previewing = this.categories[this.selected].sculptures.nodes[this.sculptureIndex].featuredImage.sourceUrl;
-                            this._fadeCurrent();
-                        }
-                    }}></iron-icon> 
+                    <iron-icon icon="chevron-left" class="${this.sculptureIndex === 0 ? 'disabled' : ''}" @click=${this._onPrevSculpture}></iron-icon> 
                     <div class="pagination"><span class="current">${this.sculptureIndex+1}</span> / <span class="total">${this.sculptureMax}</span></div> 
-                    <iron-icon icon="chevron-right" class="${this.sculptureIndex === this.sculptureMax-1 ? 'disabled' : ''}" @click=${() => {
-                        if(this.sculptureIndex+1 <= this.sculptureMax-1){
-                            this.sculptureIndex++;
-                            this.previewing = this.categories[this.selected].sculptures.nodes[this.sculptureIndex].featuredImage.sourceUrl;
-                            this._fadeCurrent();
-                        }
-                    }}></iron-icon>
+                    <iron-icon icon="chevron-right" class="${this.sculptureIndex === this.sculptureMax-1 ? 'disabled' : ''}" @click=${this._onNextSculpture}></iron-icon>
+                </div>
+                <div class="progress">
+                    <paper-progress 
+                        id="main-progress" 
+                        value=${(this.selected / this.categories.length-1) * 100}
+                        active secondary-progress=${(this.sculptureIndex / (this.sculptureMax-1)) * 100}
+                    ></paper-progress>
                 </div>
             </div>
         </div>
--- src/polymer.ts
+++ src/polymer.ts
@@ -3,3 +3,4 @@ import '@polymer/iron-icons/iron-icons';
 import '@polymer/paper-input/paper-input';
 import '@polymer/paper-spinner/paper-spinner';
 import '@polymer/paper-icon-button/paper-icon-button';
+import '@polymer/paper-progress/paper-progress';
