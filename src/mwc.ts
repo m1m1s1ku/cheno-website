b809d6735dfd199da3b8a0c937c8b1bd83964106
Léonard <leonard@bigsool.com>
2019-12-20 06:23:58 +0100

    improve contact

--- src/mwc.ts
+++ src/mwc.ts
@@ -2,3 +2,4 @@ import '@material/mwc-textfield';
 import '@material/mwc-icon';
 import '@material/mwc-textarea';
 import '@material/mwc-linear-progress';
+import '@material/mwc-button';
--- src/pages/contact.ts
+++ src/pages/contact.ts
@@ -11,8 +11,33 @@ class ContactController extends Page {
             ... super.styles,
             css`
             .contact {
+                display: grid;
+                grid-template-columns: repeat(2, 1fr);
                 padding: 2em;
                 padding-top: 6em;
+                --mdc-notched-outline-leading-width: 10px;
+                --mdc-notched-outline-trailing-border-radius: 0 10px 10px 0;
+            }
+
+            form {
+                display: flex;
+                flex-direction: column;
+                width: auto;
+            }
+
+            form .field {
+                display: flex;
+                justify-content: flex-end;
+                padding: 0;
+            }
+
+            form .field > * {
+                margin: 1em 0;
+                width: 100%;
+            }
+
+            .side.layer {
+                padding: 2em;
             }
             `
         ];
@@ -21,20 +46,30 @@ class ContactController extends Page {
     public render(): void | TemplateResult {
         return html`
         <div id="contact" class="contact" role="main">
-            <h1>Get in touch</h1>
+            <div class="side">
+            <h1>Contact</h1>
             <form>
-                <mwc-textfield
-                    label="E-mail"
-                    iconTrailing="mail_outline"
-                    outlined
-                >
-                </mwc-textfield>
-                <mwc-textarea 
-                    label="message"
-                    iconTrailing="note_Add"
-                    outlined
-                ></mwc-textarea>
+                <div class="field">
+                    <mwc-textfield
+                        label="E-mail"
+                        iconTrailing="mail_outline"
+                        outlined
+                    ></mwc-textfield>
+                </div>
+                <div class="field">
+                    <mwc-textarea 
+                        label="Message"
+                        outlined
+                    ></mwc-textarea>
+                </div>
+                <div class="field">
+                    <mwc-button raised label="Envoyer" icon="send" trailingIcon></mwc-button>
+                </div>
             </form>
+            </div>
+            <div class="side layer">
+                Otheer side
+            </div>
         </div>
         `;
     }
