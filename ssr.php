0be137729d7f921b400b2f8bb38bee18262cd1a1
Léonard <leonard@bigsool.com>
2020-04-25 23:38:55 +0200

    fix

--- ssr.php
+++ ssr.php
@@ -36,7 +36,12 @@
             $image = 'https://base.cheno.fr/wp-content/uploads/2019/12/logo-300x139.png';
         }
 
-        return "<title>$title</title><meta name='description' content='$description' /><meta property='og:title' content='$title' /><meta property='og:url' content='$url' /><meta property='og:type' content='website' /><meta property='og:image' content='$image' />";
+        $marker = '';
+        if(strpos($title, ' | Cheno') != false){
+            $marker = "<meta name='helmetized' content='done'>";
+        }
+
+        return "<title>$title</title>$marker<meta name='description' content='$description' /><meta property='og:title' content='$title' /><meta property='og:url' content='$url' /><meta property='og:type' content='website' /><meta property='og:image' content='$image' />";
     }
 
     $response = get();
