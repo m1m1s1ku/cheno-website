8a3f9db03675252c4b03498c022614f9ef034d1d
Léonard <leonard@bigsool.com>
2020-04-27 05:10:04 +0200

    added tracking

--- deploy.js
+++ deploy.js
@@ -36,6 +36,18 @@ try {
         replace.sync(options);
         console.log('replaced head');
 
+        options.from = '<!-- {{Segment}} -->';
+        options.to = `
+        <script>
+          !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.1.0";
+          analytics.load("ZeRGElPrUyplyBhgGOuDqoRBOYAd2649");
+          analytics.page();
+          }}();
+        </script>
+        `;
+        replace.sync(options);
+        console.log('added tracking');
+
         const newFile = htmlFile.replace('.html', '.php');
         renameSync(htmlFile, newFile);
         console.log('Rename ok');    
