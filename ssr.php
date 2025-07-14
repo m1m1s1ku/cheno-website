1c839cef93fd2b6da1b2537955ba74fe851c8bd2
Leonard <leonard@mimisiku.dev>
2025-07-14 20:35:00 +0200

    avoid php warnings

--- ssr.php
+++ ssr.php
@@ -48,12 +48,14 @@
     }
 
     $response = get();
-    $title = $response->title;
+
     if(strpos($title, '404') !== false){
         header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
     }
     
-    $description = $response->description;
-    $image = $response->image;
-    $url = $response->url;
+
+    $title = (property_exists($response, 'title') && isset($response->title)) ? $response->title : 'Cheno';
+    $description = (property_exists($response, 'description') && isset($response->description)) ? $response->description : "Artiste sculpteur sur Fer | Nice";
+    $image = (property_exists($response, 'image') && isset($response->image)) ? $response->image : 'https://base.cheno.fr/wp-content/uploads/2019/12/logo-300x139.png';
+    $url = (property_exists($response, 'url') && isset($response->url)) ? $response->url : 'https://www.cheno.fr' . $_SERVER['REQUEST_URI'];
 ?>
